42d374f3d80c772a3699cc98c346e8923fbd57b5
kirill8905 <liners485@gmail.com>
2022-10-22 15:11:31 +0700

    initial commit

--- mydjangoapp/mitservice/views.py
+++ mydjangoapp/mitservice/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+#from .models import Objects_KA, System_KA, Signaling, Cctv, Accesscontrol
 
-# Create your views here.
+#def mitservice(request):
+
+#    objects = Objects_KA.objects.all()
+#    systems = System_KA.objects.all()
+#    return render(request, 'service/service.html')
+
+#def sys(request):
+
+#    systems = System.objects.all()
+#    return render(request, 'service/service.html', {'system_type':systems})
+
+def mitservice(request):
+    return HttpResponse('<h2>connection completed</h2>')
